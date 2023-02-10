79b1f666beaae681675e5cff6eeef28896d72724
Shriram <88771827+Shriram-Iyer@users.noreply.github.com>
2023-02-10 14:13:01 +0530

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,159 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from "react";
+import { createDB, getData, addData, deleteData } from "./indexeddb";
+function app() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [img, setImg] = useState("");
+  const [allData, setAllData] = useState([]);
+  const [selectedRow, setSelectedRow] = useState({});
+  const [data, setData] = useState("ADD");
+  const [buttonClicked, setbutton] = useState("");
+  useEffect(() => {
+    createDB("UnifoIndexedDB", 1);
+    getAllData();
+  }, []);
+  const getAllData = async () => {
+    const Data = await getData();
+    setAllData(Data);
+    setSelectedRow({});
+    setName("");
+    setPrice("");
+    setImg("");
+    setData("ADD");
+    setbutton("");
+  };
+  const handleSubmit = async (event) => {
+    let selected, key;
+    if (data === "EDIT") {
+      selected = allData.filter((row) => row.key === selectedRow.key);
+      key = selected[0].key;
+    }
+    let obj = {
+      key: key ? key : allData[allData.length - 1].key + 1,
+      prodName: name,
+      price: price,
+      img: img
+    };
+    await addData(obj, obj.key);
+    getAllData();
+  };
+  const handleDelete = (selected) => {
+    deleteData(selected);
+    getAllData();
+  };
+  return (
+    <div className="row" style={{ padding: 100 }}>
+      <div className="col-md-6">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>SR No</th>
+              <th>Product Name</th>
+              <th>Product Price</th>
+              <th>Product Image</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allData?.map((row) => (
+              <tr key={row?.key}>
+                <td>{row?.key}</td>
+                <td>{row?.prodName}</td>
+                <td>{row?.price}</td>
+                <td>
+                  <img
+                    src={row?.img}
+                    alt={row?.key}
+                    className="img-thumbnail"
+                    style={{ height: "5rem", width: "5rem" }}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-success"
+                    onClick={() => {
+                      setSelectedRow(row);
+                      setName(row?.prodName);
+                      setPrice(row?.price);
+                      setImg(row?.img);
+                      setData("EDIT");
+                      setbutton("Edit");
+                    }}
+                  >
+                    Edit
+                  </button>{" "}
+                  &nbsp;{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      handleDelete(row.key);
+                    }}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button
+          className="btn btn-primary md-2 float-end   "
+          onClick={() => {
+            setSelectedRow({});
+            setName("");
+            setPrice("");
+            setImg("");
+            setData("ADD");
+            setbutton("Add");
+          }}
+        >
+          Add
+        </button>
+      </div>
+      {buttonClicked === "Add" || buttonClicked === "Edit" ? (
+        <div className="col-md-6">
+          <div className="card" style={{ padding: "20px" }}>
+            <h3>{data} Data</h3>
+            <div className="form-group">
+              <label>product Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label>product Price</label>
+              <input
+                type="Number"
+                name="name"
+                className="form-control"
+                onChange={(e) => setPrice(e.target.value)}
+                value={price}
+              />
+            </div>
+            <div className="form-group">
+              <label>product Image URL</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                onChange={(e) => setImg(e.target.value)}
+                value={img}
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary mt-2" onClick={handleSubmit}>
+                {data}
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+export default app;
---
+++ src/components/indexeddb/index.js
@@ -0,0 +1,103 @@
+import { item } from "../item";
+const idb = window.indexedDB;
+export const createDB = (dbname, version) => {
+  const request = idb.open(dbname, version);
+  request.onupgradeneeded = (e) => {
+    let db = request.result;
+    if (!request.result.objectStoreNames.contains("MasterData")) {
+      const objectstore = db.createObjectStore("MasterData");
+      objectstore.onsuccess = () => {
+        console.log("objectStore created!");
+      };
+      objectstore.onerror = () => {
+        console.log("error occured!");
+      };
+    }
+  };
+  request.onsuccess = (e) => {
+    let db = request.result;
+    console.log("request.onsuccess");
+    let tx = db.transaction("MasterData", "readwrite");
+    console.log("after tx");
+    console.log(tx);
+    let dataObjectStore = tx.objectStore("MasterData");
+    console.log(dataObjectStore);
+    item.forEach((i) => {
+      dataObjectStore.put(i, i.key);
+    });
+    request.result.close();
+  };
+  request.onerror = (e) => {
+    console.log("error while opening IDB!");
+  };
+};
+export const getData = async () => {
+  let DBName = "UnifoIndexedDB";
+  let request = idb.open(DBName);
+  let db,
+    dbExists = true;
+  return new Promise((resolve, reject) => {
+    //if the IDB exists return the keys else returns an empty array
+    request.onupgradeneeded = (e) => {
+      if (request.result.version === 1) {
+        dbExists = false;
+        window.indexedDB.deleteDatabase(DBName);
+        resolve([]);
+      }
+    };
+    request.onsuccess = () => {
+      if (dbExists) {
+        // alert("DB  exist")
+        db = request.result;
+        let tx = db.transaction("MasterData", "readonly");
+        let dataObjectStore = tx.objectStore("MasterData");
+        let getAllRequest = dataObjectStore.getAll();
+        getAllRequest.onsuccess = function () {
+          console.log(getAllRequest.result);
+          resolve(getAllRequest.result);
+        };
+      }
+      request.result.close();
+    };
+  });
+};
+
+export const addData = async (putData) => {
+  let DBName = "UnifoIndexedDB";
+  let version = 1;
+  let request = idb.open(DBName, version);
+  request.onsuccess = (e) => {
+    let db = request.result;
+    let dataObjectStore = db
+      .transaction("MasterData", "readwrite")
+      .objectStore("MasterData");
+    let requestAdd = dataObjectStore.put(putData, putData.key);
+    requestAdd.onerror = function (event) {
+      console.log("unable to add the key value pair ");
+    };
+    requestAdd.onsuccess = function (event) {
+      console.log("successfully added  key value pair ");
+    };
+    request.result.close();
+  };
+};
+
+export const deleteData = async (deleteKey) => {
+  let DBName = "UnifoIndexedDB";
+  let version = 1;
+  let request = indexedDB.open(DBName, version);
+  request.onsuccess = (e) => {
+    let db = request.result;
+    let dataObjectStore = db
+      .transaction("MasterData", "readwrite")
+      .objectStore("MasterData");
+    let requestDelete = dataObjectStore.delete(deleteKey);
+    requestDelete.onerror = function (event) {
+      console.log("unable to delete the key value pair ");
+    };
+    requestDelete.onsuccess = function (event) {
+      console.log("successfully deleted the key value pair ");
+    };
+    request.result.close();
+  };
+};
